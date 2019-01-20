3578ae36bc15e0b1af5b80792b8bbf46ddab922c
pizepei <848733913@qq.com>
2019-01-20 22:17:57 +0800

    db自动化维护脚本（全局）

---
+++ src/db/Maintaining.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @Author: pizepei
+ * @ProductName: PhpStorm
+ * @Created: 2019/1/20 22:13
+ * @title 自动化维护脚本（全局）
+ */
+
+namespace pizepei\model\db;
+
+
+class Maintaining
+{
+/**
+ * 提供方法 获取指定目录下（model）所有模型，然后维护表结构
+ *
+ *提供必要的DB相关的计划任务
+ *
+ */
+}
