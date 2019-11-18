0c24acafcebe551762258925eebdac5b1da4d3d1
皮泽培 <848733913@qq.com>
2019-11-18 17:54:47 +0800

    关于权限的问题，不知道修改了什么

--- src/redis/Redis.php
+++ src/redis/Redis.php
@@ -92,8 +92,6 @@ class Redis
      * @title  方法标题（一般是方法的简称）
      * @explain 一般是方法功能说明、逻辑说明、注意事项等。
      * @authTiny 微权限提供权限分配 [获取店铺所有  获取所有店铺  获取一个]
-     * @authGroup 权限分组对应文件头部 @authGroup
-     *
      * @router 方法路由一般控制器只适应(get /user/:user_id[int] ))
      */
     public static function init($config=[])
